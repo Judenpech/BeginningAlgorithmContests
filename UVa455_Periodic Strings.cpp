89cd7d71aee23c6ccf97a53dfc7d6baef7c6ec2b
J.L <jl223vy@outlook.com>
2017-12-01 23:27:58 +0800

    ce

--- UVa455_Periodic Strings.cpp
+++ UVa455_Periodic Strings.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 #define maxn 85
 using namespace std;
 int main(){
-	int n;
+	int n,flag,j;
 	char a[maxn];
 	cin>>n;
 	getchar();
 	while(n--){
-		gets(a);
-		
+		memset(a,' ',sizeof(a));
+		cin>>a;
+		for(int i=1;i<strlen(a);i++){
+			flag=0;
+			if(strlen(a)%i==0){
+				flag=1;
+				for(j=0;i+j<strlen(a);j++){
+					if(a[j]!=a[i+j]){
+						flag=0;
+						break;
+					}
+				}
+				if(flag){
+					cout<<i<<endl;
+					break;
+				}
+			}
+		}
+		if(!flag) cout<<strlen(a)<<endl;
+		if(n) cout<<endl;	
 	}
+	return 0;
 }
