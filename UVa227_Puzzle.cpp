eedc342be5c55177d1c13924596b5534387d927a
J.L <jl223vy@outlook.com>
2017-12-02 12:00:44 +0800

    upload

---
+++ UVa227_Puzzle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define maxn 5
+using namespace std;
+char a[maxn][maxn]
+int main(){
+	int cnt=1;
+	while(){
+		
+	}
+}
--- UVa455_Periodic Strings.cpp
+++ UVa455_Periodic Strings.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 #include <cstring>
-#include <string>
 #define maxn 85
 using namespace std;
 int main(){
-	int n,flag,j;
+	int n,flag;
 	char a[maxn];
 	cin>>n;
-	getchar();
 	while(n--){
-		memset(a,' ',sizeof(a));
 		cin>>a;
-		for(int i=1;i<strlen(a);i++){
-			flag=0;
+		for(int i=1;i<=strlen(a);i++){
 			if(strlen(a)%i==0){
 				flag=1;
-				for(j=0;i+j<strlen(a);j++){
-					if(a[j]!=a[i+j]){
+				for(int j=i;j<strlen(a);j++){
+					if(a[j]!=a[j%i]){
 						flag=0;
 						break;
 					}
 				}
 				if(flag){
 					cout<<i<<endl;
+					if(n) cout<<endl;
 					break;
 				}
 			}
 		}
-		if(!flag) cout<<strlen(a)<<endl;
-		if(n) cout<<endl;	
 	}
 	return 0;
 }
