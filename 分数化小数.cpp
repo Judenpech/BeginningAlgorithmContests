eb67521da682ea069cdfbdc6a5fe48b1fa15dbf3
J.L <jl223vy@outlook.com>
2017-12-04 21:26:27 +0800

    upload

--- 分数化小数.cpp
+++ 分数化小数.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 	int a,b,c,cnt=1;
 	while(cin>>a>>b>>c && a!=0 && b!=0 && c!=0){
-		cout<<"Case "<<cnt++<<": "<<setprecision(c)<<(double)a/b<<endl;
+		cout<<"Case "<<cnt++<<": "<<setprecision(c)<<fixed<<(double)a/b<<endl;
 	}	
 }
