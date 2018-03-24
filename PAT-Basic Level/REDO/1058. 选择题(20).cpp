203d39b183244bbc46ebb01af89fe6aee5a3f894
J.L <jl223vy@outlook.com>
2018-03-24 15:19:07 +0800

    upload

--- PAT-Basic Level/REDO/1058. 选择题(20).cpp
+++ PAT-Basic Level/REDO/1058. 选择题(20).cpp
@@ -1,54 +1,71 @@
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 using namespace std;
 struct Ti{
+	int bh; //编号
 	int mf; //满分值
 	int xxgs; //选项个数
 	int zqxxgs; //正确选项个数
-	char zqxs[7]; //正确选项
+	string zqxs; //正确选项
 	int cuo=0; //错误次数
 }ti[105];
+bool cmp(Ti a,Ti b){
+	if(a.cuo==b.cuo){
+		return a.bh<b.bh;
+	}else{
+		return a.cuo>b.cuo;
+	}
+}
 int main(){
 	int n,m;
 	cin>>n>>m;
+	char t;
 	for(int i=0;i<m;i++){
 		cin>>ti[i].mf>>ti[i].xxgs>>ti[i].zqxxgs;
+		ti[i].bh=i+1;
 		for(int j=0;j<ti[i].zqxxgs;j++){
-			cin>>ti[i].zqxs[j];
+			cin>>t;
+			ti[i].zqxs+=t;
 		}
 	}
+	getchar();
 	int flag=1;
 	string s;
 	int k;//选中的选项个数
-	char xx; //选项
 	for(int i=0;i<n;i++){
 		int defen=0;
 		getline(cin,s);
-		for(int j=0;j<s.length();j++){
+		for(int j=0,mm=0;j<s.length();j++){
+			string xx; //选项
 			if(isdigit(s[j])){
-				int cnt=0;
 				k=s[j]-'0';
-				for(int q=j+2;s[q]!=')';q++){
-					xx=
-					for(int p=0;p<ti[j].zqxxgs;p++){
-						if(xx=ti[j].zqxs[p]){
-							cnt++;
-							break;
-						}
-					}
+				for(int q=j+2,p=0;p<k;q+=2,p++){
+					xx+=s[q];
 				}
+				if(k!=ti[mm].zqxxgs || xx!=ti[mm].zqxs){
+					ti[mm].cuo++;
+					flag=0;
+				}else{
+					defen+=ti[mm].mf;
+				}
+				mm++;
 			}
-			
-			
-			if(k!=ti[j].zqxxgs || cnt!=ti[j].zqxxgs){
-				ti[j].cuo++;
-			}else{
-				defen+=ti[j].mf;
-			}
-			getchar();
 		}
 		cout<<defen<<endl;
 	}
-	//if(flag) cout<<"Too simple"<<endl;
+	sort(ti,ti+m,cmp);
+	if(flag) cout<<"Too simple"<<endl;
+	else{
+		cout<<ti[0].cuo<<" "<<ti[0].bh;
+		for(int i=1;i<m;i++){
+			if(ti[i].cuo==ti[0].cuo){
+				cout<<" "<<ti[i].bh;
+			}else{
+				break;
+			}
+
+		}
+	}
 	return 0;
 }
--- PAT-Basic Level/REDO/1069. 微博转发抽奖(20).cpp
+++ PAT-Basic Level/REDO/1069. 微博转发抽奖(20).cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 #include <string>
-using namespace std;//第三个测试点答案错误
+#include <map>
+using namespace std;
 int main(){
 	int m,n,s;
 	cin>>m>>n>>s;
-	string t,a[1005];
-	int len=0,f2=0;//len为记录已中奖数组长度，f2标记已中过奖
-	for(int i=1,j=s;i<=m;i++){
+	map<string,int> mapp;
+	string t;
+	int flag=1;
+	for(int i=1;i<=m;i++){
 		cin>>t;
-		if(i==j || f2){
-			int flag=1;
-			for(int k=0;k<len;k++){
-				if(a[k]==t){
-					flag=0;
-					f2=1;
-					break;
-				}
-			}
-			if(flag){
-				cout<<t<<endl;
-				j+=n;
-				f2=0;
-				a[len++]=t;
-			}
+		if(mapp[t]==1) s+=1;
+		if(i==s && mapp[t]==0){
+			mapp[t]=1;
+			flag=0;
+			cout<<t<<endl;
+			s+=n;
 		}
 	}
-	if(s>m){
+	if(flag){
 		cout<<"Keep going..."<<endl;
 	}
 	return 0;
