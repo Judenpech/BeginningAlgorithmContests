5921473a9f288ea018eb59eba4e937f44fa4bd09
J.L <jl223vy@outlook.com>
2018-05-24 22:08:54 +0800

    upload

---
+++ LeetCode/181. 超过经理收入的员工.sql
@@ -0,0 +1,7 @@
+﻿# Write your MySQL query statement below
+SELECT e1.Name Employee
+    FROM Employee e1
+    WHERE e1.Salary>(
+    SELECT e2.Salary
+    From Employee e2
+    WHERE e2.Id=e1.ManagerId);
---
+++ LeetCode/182. 查找重复的电子邮箱.sql
@@ -0,0 +1,5 @@
+﻿# Write your MySQL query statement below
+SELECT p.Email
+    From Person p
+    GROUP BY p.Email
+    HAVING COUNT(p.Email)>1;
