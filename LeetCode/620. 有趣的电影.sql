c5f48f5f1b410407e5a4495df8dfa2aa7f5c1702
J.L <jl223vy@outlook.com>
2018-05-25 11:16:58 +0800

    upload

---
+++ LeetCode/620. 有趣的电影.sql
@@ -0,0 +1,5 @@
+﻿# Write your MySQL query statement below
+SELECT c.id,c.movie,c.description,c.rating
+    FROM cinema c
+    WHERE MOD(c.id,2)=1 AND c.description!="boring"
+    ORDER BY c.rating DESC;
---
+++ LeetCode/627. 交换工资.sql
@@ -0,0 +1,4 @@
+﻿# Write your MySQL query statement below
+UPDATE salary s
+SET s.sex=IF(s.sex='f','m','f' );
+# SET s.sex=(CASE WHEN s.sex='f' THEN 'm' ELSE 'f' END);
