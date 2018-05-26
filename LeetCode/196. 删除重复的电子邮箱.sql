e1a53ec98bfb79f7cbd94230fa4cac6801815d8d
J.L <jl223vy@outlook.com>
2018-05-26 21:25:56 +0800

    upload

---
+++ LeetCode/196. 删除重复的电子邮箱.sql
@@ -0,0 +1,9 @@
+﻿# Write your MySQL query statement below
+DELETE FROM Person 
+    WHERE Id NOT IN(
+        SELECT Id
+        FROM(
+            SELECT MIN(Id) Id
+            FROM Person 
+            GROUP BY Email)p);
+        
