f8f46faa47171707e5508879cd31d39495dad2e6
J.L <jl223vy@outlook.com>
2018-05-28 19:04:20 +0800

    upload

---
+++ LeetCode/176. 第二高的薪水.sql
@@ -0,0 +1,12 @@
+﻿Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+
+-----
+
+# Write your MySQL query statement below
+SELECT MAX(e1.Salary) SecondHighestSalary
+	FROM Employee e1
+		WHERE e1.Salary<(SELECT MAX(Salary) FROM Employee);
---
+++ LeetCode/626. 换座位.sql
@@ -0,0 +1,19 @@
+﻿Create table If Not Exists seat(id int, student varchar(255))
+Truncate table seat
+insert into seat (id, student) values ('1', 'Abbot')
+insert into seat (id, student) values ('2', 'Doris')
+insert into seat (id, student) values ('3', 'Emerson')
+insert into seat (id, student) values ('4', 'Green')
+insert into seat (id, student) values ('5', 'Jeames')
+
+
+-----
+
+# Write your MySQL query statement below
+SELECT s.id,s.student
+FROM (SELECT id-1 id,student FROM seat WHERE MOD(id,2)=0 
+      UNION
+	  SELECT id+1 id,student FROM seat WHERE MOD(id,2)=1 AND id!=(SELECT COUNT(*) FROM seat) 
+      UNION
+	  SELECT id,student FROM seat WHERE MOD(id,2)=1 AND id=(SELECT COUNT(*) FROM seat)) s
+ORDER BY id; 
