496a80df8b1a497a3ad7df5ad0a1b383b1d13cac
J.L <jl223vy@outlook.com>
2018-05-29 20:08:22 +0800

    upload

---
+++ LeetCode/178. 分数排名.sql
@@ -0,0 +1,15 @@
+﻿Create table If Not Exists Scores (Id int, Score DECIMAL(3,2))
+Truncate table Scores
+insert into Scores (Id, Score) values ('1', '3.5')
+insert into Scores (Id, Score) values ('2', '3.65')
+insert into Scores (Id, Score) values ('3', '4.0')
+insert into Scores (Id, Score) values ('4', '3.85')
+insert into Scores (Id, Score) values ('5', '4.0')
+insert into Scores (Id, Score) values ('6', '3.65')
+
+-----
+
+# Write your MySQL query statement below
+SELECT s.Score,(SELECT COUNT(DISTINCT s1.Score) FROM Scores s1 WHERE s1.Score>=s.Score) Rank
+FROM Scores s
+ORDER BY Rank
