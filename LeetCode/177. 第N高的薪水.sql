91ed9ccf4e495c5e55acdeb1607b9359788b78aa
J.L <jl223vy@outlook.com>
2018-05-30 20:15:55 +0800

    upload

---
+++ LeetCode/177. 第N高的薪水.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    declare n1 int;
+    set n1=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select Salary from 
+      (select distinct Salary from Employee) e
+      order by Salary desc 
+      limit n1,1
+  );
+END
