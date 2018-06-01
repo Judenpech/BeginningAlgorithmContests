84beeb92c43047b3e81bc545b5ae5f203f6c7a8a
J.L <jl223vy@outlook.com>
2018-06-01 20:20:14 +0800

    upload

---
+++ LeetCode/184. 部门工资最高的员工.sql
@@ -0,0 +1,17 @@
+﻿Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int)
+Create table If Not Exists Department (Id int, Name varchar(255))
+Truncate table Employee
+insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Henry', '80000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Sam', '60000', '2')
+insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (Id, Name) values ('1', 'IT')
+insert into Department (Id, Name) values ('2', 'Sales')
+
+-----
+
+# Write your MySQL query statement below
+select d.Name Department,e1.Name Employee,e1.Salary Salary
+from Employee e1 join Department d on e1.DepartmentId=d.Id
+where e1.Salary in(select max(e2.Salary) from Employee e2 where e1.DepartmentId=e2.DepartmentId)
