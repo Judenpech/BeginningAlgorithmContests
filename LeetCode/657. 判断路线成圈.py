6bd0b6088fef36ac99cd3c070c0e40879a1c790f
J.L <jl223vy@outlook.com>
2018-06-16 15:29:14 +0800

    upload

---
+++ LeetCode/657. 判断路线成圈.py
@@ -0,0 +1,39 @@
+﻿class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        cntR=moves.count("R")
+        cntL=moves.count("L")
+        cntU=moves.count("U")
+        cntD=moves.count("D")
+        if cntR==cntL and cntU==cntD:
+            return True
+        else:
+            return False
+
+def stringToString(input):
+    return input[1:-1].decode('string_escape')
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            moves = stringToString(line);
+            
+            ret = Solution().judgeCircle(moves)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
