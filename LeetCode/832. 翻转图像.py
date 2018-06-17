b75b3782f34d547ab7945c5bf7620e9b9484b2ce
J.L <jl223vy@outlook.com>
2018-06-17 13:22:09 +0800

    upload

---
+++ LeetCode/832. 翻转图像.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in A:
+            i.reverse()
+            for j in range(len(i)):
+                if i[j]==0:
+                    i[j]=1
+                else:
+                    i[j]=0
+        return A
+
