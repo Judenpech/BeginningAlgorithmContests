1f927f3366045501ad1a2563799adad2c003aed8
J.L <jl223vy@outlook.com>
2018-06-18 10:46:51 +0800

    upload

---
+++ LeetCode/461. 汉明距离.py
@@ -0,0 +1,33 @@
+﻿class Solution:
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        #异或运算：同0异1
+        return str(bin(x^y)).count('1')
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            x = int(line);
+            line = next(lines)
+            y = int(line);
+            
+            ret = Solution().hammingDistance(x, y)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
