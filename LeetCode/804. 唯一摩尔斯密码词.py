c41d5063b58c08bd61000915d925e8830f5a975e
J.L <jl223vy@outlook.com>
2018-06-19 13:01:12 +0800

    upload

---
+++ LeetCode/804. 唯一摩尔斯密码词.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        d = {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.",
+             "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.",
+             "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-",
+             "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--.."}
+        cnt=set()
+        for word in words:
+            morseCode = ""
+            for c in word:
+                morseCode+=d[c]
+            cnt.add(morseCode)
+        return len(cnt)
+
