1bea7000901978798c374aaf29b4cfdb47c1876e
J.L <jl223vy@outlook.com>
2018-06-20 21:55:58 +0800

    upload

---
+++ LeetCode/476. 数字的补数.py
@@ -0,0 +1,36 @@
+﻿class Solution:
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        a=bin(num)[2:]
+        re=""
+        for i in a:
+            if i=="1":
+                re+="0"
+            else:
+                re+="1"
+        return int(re,2)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            num = int(line);
+            
+            ret = Solution().findComplement(num)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
