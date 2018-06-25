1b05966cd48b640930caf822b2e62977df431523
J.L <jl223vy@outlook.com>
2018-06-25 18:23:33 +0800

    upload

---
+++ LeetCode/344. 反转字符串.py
@@ -0,0 +1,33 @@
+﻿class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        rev=s[-1::-1]
+        return rev
+
+def stringToString(input):
+    return input[1:-1].decode('string_escape')
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            s = stringToString(line);
+            
+            ret = Solution().reverseString(s)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
