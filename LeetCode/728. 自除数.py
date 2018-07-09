db70372b0aafa45f0a1ed6bf461447436c7e01a9
J.L <jl223vy@outlook.com>
2018-07-09 17:20:57 +0800

    upload

---
+++ LeetCode/728. 自除数.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        l=[]
+        for i in range(left,right+1):
+            flag=1
+            for n in str(i):
+                if n=='0':
+                    flag=0
+                    break
+                if i%int(n)!=0:
+                    flag=0
+                    break
+            if flag:
+                l.append(i)
+        return l
+        
