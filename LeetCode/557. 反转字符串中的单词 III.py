1bb50fa7a413ba058b109cd6fcf17ce627ecdc0c
J.L <jl223vy@outlook.com>
2018-07-10 08:30:18 +0800

    upload

---
+++ LeetCode/557. 反转字符串中的单词 III.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        l=s.split()
+        ll=[]
+        for i in l:
+            ll.append(i[-1::-1])
+        retn=" ".join(ll)
+        return retn
+        
+        
