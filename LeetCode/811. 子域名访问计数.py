df93308f55f6361c88d0f58e67301c04d22543fc
J.L <jl223vy@outlook.com>
2018-07-13 20:51:48 +0800

    upload

---
+++ LeetCode/811. 子域名访问计数.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        domain = {}
+        for d in cpdomains:
+            l = d.split()
+            v = int(l[0])
+            begin = 0
+            l[1] = " " + l[1]
+            while begin != -1:
+                key = l[1][begin + 1:]
+                domain[key] = domain.get(key, 0) + v
+                begin = l[1].find('.', begin + 1)
+        ret = []
+        for k in domain.keys():
+            ret.append(str(domain[k]) + " " + str(k))
+        return ret
