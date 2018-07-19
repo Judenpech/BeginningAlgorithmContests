2461d3f922181221281782073128ddf591906646
J.L <jl223vy@outlook.com>
2018-07-19 09:03:57 +0800

    upload

---
+++ LeetCode/821. 字符的最短距离.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        ret=[]
+        lens=len(S)
+        for i in range(lens):
+            right=S.find(C,i)
+            if right==-1:
+                right=100002
+            else:
+                right-=i
+            left=S[:i+1].rfind(C)
+            if left==-1:
+                left=100002
+            else:
+                left=i-left
+            ret.append(min(right,left))
+        return ret
+        
