a89530db807db98393e1e1bb9031a6280c5cb8eb
J.L <jl223vy@outlook.com>
2018-07-20 09:25:02 +0800

    upload

---
+++ LeetCode/693. 交替位二进制数.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        binaryStr = ""
+        while n != 0:
+            binaryStr = binaryStr + str(n % 2)
+            n = n // 2
+        lens = len(binaryStr)
+        for i in range(lens - 1):
+            if binaryStr[i] == binaryStr[i + 1]:
+                return False
+        return True
---
+++ LeetCode/709. 转换成小写字母.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return(str.lower())
+        
