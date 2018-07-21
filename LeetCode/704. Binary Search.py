bcc824956355aa0a9ebc4891ddb2e996bb67243f
J.L <jl223vy@outlook.com>
2018-07-21 14:14:52 +0800

    upload

---
+++ LeetCode/704. Binary Search.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        min = 0
+        max = len(nums) - 1
+        while min <= max:
+            mid = min + (max - min) // 2
+            if nums[mid] < target:
+                min = mid + 1
+            elif nums[mid] > target:
+                max = mid - 1
+            else:
+                return mid
+        return -1
