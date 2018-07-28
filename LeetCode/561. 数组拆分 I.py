41a72aff676af4cb6a4470342200f55797086ffa
J.L <jl223vy@outlook.com>
2018-07-28 17:29:12 +0800

    upload

---
+++ LeetCode/561. 数组拆分 I.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        lens=len(nums)
+        ssum=0
+        for i in range(0,lens,2):
+            mmin=min(nums[i],nums[i+1])
+            ssum+=mmin
+        return ssum
