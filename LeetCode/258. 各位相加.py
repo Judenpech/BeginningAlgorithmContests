b0fb4f6268f6d69ea5bfc76384803d22007b1a89
J.L <jl223vy@outlook.com>
2018-07-31 10:01:38 +0800

    upload

---
+++ LeetCode/258. 各位相加.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num>=10:
+            t=num
+            ssum=0
+            while t!=0:
+                ssum+=t%10
+                t//=10
+            num=ssum
+        return num
+        
