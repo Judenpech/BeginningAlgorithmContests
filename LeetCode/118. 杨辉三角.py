ec2825480708e3d8245b46066686dcedb621663e
jl223vy <jl223vy@outlook.com>
2018-08-02 22:39:17 +0800

    upload

---
+++ LeetCode/118. 杨辉三角.py
@@ -0,0 +1,17 @@
+﻿class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        l=[]
+        for i in range(numRows):
+            t=[]
+            for j in range(i+1):
+                if j==0 or j==i:
+                    t.append(1)
+                else:
+                    t.append(l[i-1][j]+l[i-1][j-1])
+            l.append(t)
+        return l
+        
