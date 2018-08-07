6d31e0017b5fb8c7c5bbaaa931c93df4a41055c4
jl223vy <jl223vy@outlook.com>
2018-08-07 21:54:43 +0800

    upload

---
+++ LeetCode/566. 重塑矩阵.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        rr = len(nums)
+        cc = len(nums[0])
+        retl = []
+        if rr * cc < r * c:
+            return nums
+        else:
+            t = []
+            for i in range(rr):
+                for j in range(cc):
+                    t.append(nums[i][j])
+            m = 0
+            for i in range(r):
+                retl.append(t[m:m + c])
+                m += c
+        return retl
