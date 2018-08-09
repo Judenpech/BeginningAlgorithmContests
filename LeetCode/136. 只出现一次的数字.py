c4ff814aee6d056bbc6cd907045cc9529ca9f975
jl223vy <jl223vy@outlook.com>
2018-08-09 20:42:59 +0800

    upload

---
+++ LeetCode/136. 只出现一次的数字.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lens=len(nums)
+        t=0
+        for i in range(lens):
+           t^=nums[i]
+        return t
