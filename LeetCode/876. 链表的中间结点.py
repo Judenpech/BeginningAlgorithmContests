741d2003671d809d46f50c530fb72e47cd434fbb
jl223vy <jl223vy@outlook.com>
2018-08-11 15:21:33 +0800

    upload

---
+++ LeetCode/876. 链表的中间结点.py
@@ -0,0 +1,19 @@
+﻿# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pa=pb=head
+        while pa and pb.next:
+            if pb.next.next==None:
+                return pa.next
+            pa=pa.next
+            pb=pb.next.next
+        return pa
