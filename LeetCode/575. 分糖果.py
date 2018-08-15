c8b2b2e06627300dd2addca1baf25391205ee435
jl223vy <jl223vy@outlook.com>
2018-08-15 20:27:05 +0800

    upload

---
+++ LeetCode/575. 分糖果.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        candy = len(set(candies))
+        half = len(candies) // 2
+        if candy < half:
+            return candy
+        else:
+            return half
+        
